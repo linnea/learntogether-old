146905e397e749ee7d33490d2f6e7e6d9596f1a6
jkprow <jkprow@gmail.com>
2014-11-16 21:35:27 -0800

    proof of concept for modal working, just need to integrate it, brb ice cream

--- client/private/apps/admin/js/admin.js
+++ client/private/apps/admin/js/admin.js
@@ -1,7 +1,8 @@
 (function() {
-    var admin = angular.module('admin', []);
+    var admin = angular.module('admin', ['ui.bootstrap']);
 
-    admin.controller('DatabaseController', ['$http', function($http) {
+    //admin.controller('DatabaseController', ['$http', function($http) {
+    admin.controller('DatabaseController', function ($http) {
         var db = this;
         this.users = [];
 
@@ -16,10 +17,49 @@
 
         this.deleteUser = function(id) {
             $http.delete('/api/users/' + id).success(function (res) {
-                console.log("clicky!");
+                console.log(res.data.user);
                 db.getUsers();
             });
         };
+    });
 
-    }]);
+    admin.controller('ModalController', function($modal, $log) {
+        var modal = this;
+        this.items = ['item', 'item2', 'item3'];
+
+        this.open = function (size) {
+            var modalInstance = $modal.open({
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceController as mic',
+                size: size,
+                resolve: {
+                    items: function () {
+                        return modal.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                modal.selected = selectedItem;
+            }, function() {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+    });
+
+    admin.controller('ModalInstanceController', function ($modalInstance, items) {
+        var modalInstance = this;
+        this.items = items;
+        this.selected = {
+            item: this.items[0]
+        };
+
+        this.ok = function () {
+            $modalInstance.close(modalInstance.selected.item);
+        };
+
+        this.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    });
 })();
