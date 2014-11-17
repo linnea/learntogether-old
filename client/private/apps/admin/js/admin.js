34b87882a2061801153246dbb8b058e90f2a2722
jkprow <jkprow@gmail.com>
2014-11-17 15:55:28 -0800

    Admin CREATE is working with some caveats: On create, page is reloaded with location.reload() (LOL).  Really need to put CRUD functions in a service The code is kind of 'sketti right now, need to workshop this a little bit.  Don't want to attempt UPDATE until this is straight Changed POST method on the API router to auto-increment user id when added

--- client/private/apps/admin/js/admin.js
+++ client/private/apps/admin/js/admin.js
@@ -5,6 +5,7 @@
     admin.controller('DatabaseController', function ($http) {
         var db = this;
         this.users = [];
+        //this.createData = {};
 
         this.getUsers = function() {
             $http.get('/api/users').success(function (res) {
@@ -23,39 +24,51 @@
         };
     });
 
-    admin.controller('ModalController', function($modal, $log) {
+    admin.controller('ModalController', function($modal, $log, $http) {
         var modal = this;
-        this.items = ['item', 'item2', 'item3'];
+        //this.items = ['item', 'item2', 'item3'];
+        this.newUser = {};
+
+        this.addUser = function() {
+            $http.post('/api/users', modal.newUser).success(function (res) {
+                console.log(modal.newUser);
+                console.log(res);
+                location.reload();
+
+                // Need to figure out how to call db.getUsers from here
+                //getUsers();
+            });
+        };
 
         this.open = function (size) {
             var modalInstance = $modal.open({
-                templateUrl: 'myModalContent.html',
+                templateUrl: 'modalContent.html',
                 controller: 'ModalInstanceController as mic',
-                size: size,
                 resolve: {
-                    items: function () {
-                        return modal.items;
+                    newUser: function() {
+                        return modal.newUser;
                     }
                 }
             });
 
-            modalInstance.result.then(function (selectedItem) {
-                modal.selected = selectedItem;
+            modalInstance.result.then(function (/*selectedItem*/ newUser) {
+                modal.addUser(newUser);
             }, function() {
                 $log.info('Modal dismissed at: ' + new Date());
             });
         };
     });
 
-    admin.controller('ModalInstanceController', function ($modalInstance, items) {
+    admin.controller('ModalInstanceController', function ($modalInstance, /*items*/ newUser) {
         var modalInstance = this;
-        this.items = items;
-        this.selected = {
+        //this.items = items;
+        this.newUser = newUser;
+        /*this.selected = {
             item: this.items[0]
-        };
+        };*/
 
         this.ok = function () {
-            $modalInstance.close(modalInstance.selected.item);
+            $modalInstance.close(modalInstance.newUser);
         };
 
         this.cancel = function () {
