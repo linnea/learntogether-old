fde901e69c85318319954614688bec40425b8c5d
fmnguyen <fmnguyen@uw.edu>
2014-11-20 17:18:53 -0800

    add isAdmin columns and auto-insert root in Users table

--- scripts/db.sql
+++ scripts/db.sql
@@ -10,9 +10,11 @@ CREATE TABLE IF NOT EXISTS "Users" (
 	"name" VARCHAR(255), 
 	"email" VARCHAR(255) UNIQUE, 
 	"password" VARCHAR(255), 
+	"isAdmin" BOOLEAN,
 	"createdAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
 	"updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
 	PRIMARY KEY ("id")
 );
+INSERT INTO "Users" (name, email, password, "isAdmin", "createdAt", "updatedAt") VALUES ('root', 'root', 'root', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 GRANT ALL PRIVILEGES ON TABLE "Users" TO root;
 GRANT ALL PRIVILEGES ON "Users_id_seq" TO root;
--- scripts/dbconfig.sh
+++ scripts/dbconfig.sh
@@ -20,6 +20,8 @@ CREATE TABLE IF NOT EXISTS "Users" (
 	"updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
 	PRIMARY KEY ("id")
 );
+
+INSERT INTO "Users" (name, email, password, "isAdmin", "createdAt", "updatedAt") VALUES ('root', 'root', 'root', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 GRANT ALL PRIVILEGES ON TABLE "Users" TO root;
 GRANT ALL PRIVILEGES ON "Users_id_seq" TO root;
 EOF
