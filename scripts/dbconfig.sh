2c1c29f661c38b2427b03d6e352526a32f48d913
fmnguyen <fmnguyen@uw.edu>
2014-12-12 18:50:45 -0800

    Fix hashed password error

--- scripts/dbconfig.sh
+++ scripts/dbconfig.sh
@@ -1,34 +1,4 @@
 #!/bin/bash
 
 ## ADD ROOT USER, CREATE SYNTHERGIZEDB DATABASE, GRANT ALL PRIVILEGES
-<<<<<<< HEAD
 psql < ./scripts/db.sql 
-=======
-psql -h localhost << EOF
-CREATE USER root WITH PASSWORD 'root';
-CREATE DATABASE learntogether;
-GRANT ALL PRIVILEGES ON DATABASE learntogether to root;
-\q
-EOF
-
-psql learntogether << EOF
-CREATE SCHEMA IF NOT EXISTS "core";
-CREATE TABLE IF NOT EXISTS "Users" (
-	"id"   SERIAL, 
-	"firstName" VARCHAR(255),
-	"lastName" VARCHAR(255),
-	"email" VARCHAR(255) UNIQUE, 
-	"password" VARCHAR(255), 
-	"isAdmin" BOOLEAN,
-	"createdAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
-	"updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
-	"isApproved" BOOLEAN,
-	"role" integer NOT NULL,
-	PRIMARY KEY ("id")
-);
-INSERT INTO "Users" ("firstName", "lastName", email, password, "isAdmin", "createdAt", "updatedAt", "isApproved", role) 
-VALUES ('root', 'root', 'root', '$2a$08$qOepzilN.F074dhepDHv8unJ2VWqBj9ctMCKUwqwrvad1.Qsd2Pcu', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE, 300);
-GRANT ALL PRIVILEGES ON TABLE "Users" TO root;
-GRANT ALL PRIVILEGES ON "Users_id_seq" TO root;
-EOF
->>>>>>> alex/master
