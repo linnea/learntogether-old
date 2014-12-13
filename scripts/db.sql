6168d128c2d684224370c38df1ec9b0019833c66
fmnguyen <fmnguyen@uw.edu>
2014-12-12 16:12:53 -0800

    Changed attribute names

--- scripts/db.sql
+++ scripts/db.sql
@@ -7,14 +7,14 @@ GRANT ALL PRIVILEGES ON DATABASE learntogether to root;
 CREATE SCHEMA IF NOT EXISTS "core";
 CREATE TABLE IF NOT EXISTS "Users" (
 	"id"   SERIAL, 
-	"userFirstName" VARCHAR(255),
-	"userLastName" VARCHAR(255),
+	"firstName" VARCHAR(255),
+	"lastName" VARCHAR(255),
 	"email" VARCHAR(255) UNIQUE, 
 	"password" VARCHAR(255), 
 	"isAdmin" BOOLEAN,
 	"createdAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
 	"updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
-	"isVerified" BOOLEAN,
+	"isApproved" BOOLEAN,
 	"role" integer NOT NULL
 	PRIMARY KEY ("id")
 );
