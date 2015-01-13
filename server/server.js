bf85cff1082091fc133a99db405b23e35f569ab5
Alex Burner <alex.burner@gmail.com>
2015-01-13 02:20:13 -0800

    updating server todo

--- server/server.js
+++ server/server.js
@@ -79,22 +79,5 @@ module.exports = new Promise(function (resolve, reject) {
  * - graceful shutdown
  * - logging (http://blog.nodejs.org/2012/03/28/service-logging-in-json-with-bunyan/)
  * - forever (https://www.npmjs.com/package/forever)
- *
- *	!!! use node-postgres
- *		- ensure db connection before starting app
- *		- allow config/modules.js to grab its own list of module names
- *
- *
- * hey also, should we really be using postgres?
- * we've been testing it out and found it difficult to work with
- * (aka PG Commander vs the buttery ease of Sequel Pro)
- * mysql has a massive community, broader adoption (facebook, wordpress)
- *
- * ok no wait, looks like pg is the most reliable, compliant, and ACID db option
- * https://www.digitalocean.com/community/tutorials/sqlite-vs-mysql-vs-postgresql-a-comparison-of-relational-database-management-systems
- *
- * but replication/clustering will be a thing to figure out
- * https://wiki.postgresql.org/wiki/Replication,_Clustering,_and_Connection_Pooling
- *
  */
 
