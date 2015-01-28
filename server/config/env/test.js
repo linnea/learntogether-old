4440aadbf68b9f9833f9b0c4afb46da59e4ac08c
Alex Burner <alex.burner@gmail.com>
2015-01-27 22:02:21 -0800

    adding SSL cert for testing server

--- server/config/env/test.js
+++ server/config/env/test.js
@@ -6,8 +6,8 @@ module.exports = {
 	domain: "menrva.io",
 	http_port: process.env.HTTP_PORT || 3000,
 	https_port: process.env.HTTPS_PORT || 4000,
-	private_key: fs.readFileSync(__dirname + '/ssl/local-key.pem', 'utf-8'),
-	public_cert: fs.readFileSync(__dirname + '/ssl/local-cert.pem', 'utf-8'),
+	private_key: fs.readFileSync(__dirname + '/ssl/test-key.pem', 'utf-8'),
+	public_cert: fs.readFileSync(__dirname + '/ssl/test-cert.pem', 'utf-8'),
 	db: {
 		database: "learntogether",
 		username: "root",
